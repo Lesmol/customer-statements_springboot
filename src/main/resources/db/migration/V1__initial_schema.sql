6cfbfd7da69df3be8b7db9afe9f8e80acd4ebd21
Lesedi Molemi <molemilesedi@gmail.com>
2026-08-01 12:37:22 +0200

    cleanup schema id definition

--- src/main/resources/db/migration/V1__initial_schema.sql
+++ src/main/resources/db/migration/V1__initial_schema.sql
@@ -1,6 +1,6 @@
 CREATE TABLE users
 (
-    id         UUID PRIMARY KEY      DEFAULT gen_random_uuid(),
+    id         UUID PRIMARY KEY,
     username   VARCHAR(255) NOT NULL UNIQUE,
     password   VARCHAR(255) NOT NULL,
     created_at TIMESTAMPTZ  NOT NULL DEFAULT now()
@@ -8,14 +8,14 @@ CREATE TABLE users
 
 CREATE TABLE documents
 (
-    id          UUID PRIMARY KEY     DEFAULT gen_random_uuid(),
+    id          UUID PRIMARY KEY,
     user_id     UUID        NOT NULL REFERENCES users (id),
     uploaded_at TIMESTAMPTZ NOT NULL DEFAULT now()
 );
 
 CREATE TABLE document_retrievals
 (
-    id           UUID PRIMARY KEY     DEFAULT gen_random_uuid(),
+    id           UUID PRIMARY KEY,
     document_id  UUID        NOT NULL REFERENCES documents (id),
     retrieved_at TIMESTAMPTZ NOT NULL DEFAULT now(),
     expired_at   TIMESTAMPTZ NOT NULL
