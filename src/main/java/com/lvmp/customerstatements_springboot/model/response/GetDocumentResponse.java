ce70f44c0358d4f8c9a0b5edd09b44935ba95d1a
Lesedi Molemi <molemilesedi@gmail.com>
2026-08-02 14:38:15 +0200

    update GetDocumentResponse to allow deserialization

--- src/main/java/com/lvmp/customerstatements_springboot/model/response/GetDocumentResponse.java
+++ src/main/java/com/lvmp/customerstatements_springboot/model/response/GetDocumentResponse.java
@@ -1,12 +1,16 @@
 package com.lvmp.customerstatements_springboot.model.response;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.time.Instant;
 
 @Data
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class GetDocumentResponse {
     private String url;
     private Instant expiresAt;
