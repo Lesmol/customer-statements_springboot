cb4f4c31a9129f59d4316c40eecac9eb99c904d8
Lesedi Molemi <molemilesedi@gmail.com>
2026-08-21 19:47:01 +0200

    initial commit to decouple services

---
+++ login-service/src/main/java/com/lvmp/loginservice/LoginServiceApplication.java
@@ -0,0 +1,13 @@
+package com.lvmp.loginservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoginServiceApplication {
+
+    static void main(String[] args) {
+        SpringApplication.run(LoginServiceApplication.class, args);
+    }
+
+}
