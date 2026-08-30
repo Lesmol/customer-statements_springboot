ac79731eb544971bcd47d326aa40cf4d179a9d18
Lesedi Molemi <molemilesedi@gmail.com>
2026-08-30 17:10:34 +0200

    remove auth logic from statement-service

--- statement-service/src/main/java/com/lvmp/customerstatements_springboot/exception/UserAlreadyExistsException.java
+++
@@ -1,7 +0,0 @@
-package com.lvmp.customerstatements_springboot.exception;
-
-public class UserAlreadyExistsException extends RuntimeException {
-    public UserAlreadyExistsException(String message) {
-        super(message);
-    }
-}
